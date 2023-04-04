67b9163601bdb9b80488bf5c02d7d14fcd0fea28
ashok poudel <ashok.poudel@gmail.com>
2023-04-04 20:29:03 +0545

    set create role to false

--- variables.tf
+++ variables.tf
@@ -5,5 +5,5 @@ variable "lambda_role_name" {
 variable "create_iam_resources" {
   description = "Whether to create new IAM role and policy or use existing ones"
   type        = bool
-  default     = true
+  default     = false
 }
