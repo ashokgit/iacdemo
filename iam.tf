2d5256dfa2fa9788e23d24c9e617b24647c98d6e
ashok poudel <ashok.poudel@gmail.com>
2023-04-04 16:01:04 +0545

    line ma aayo

--- iam.tf
+++ iam.tf
@@ -1,43 +1,22 @@
 resource "aws_iam_role" "lambda_role" {
-  name = "iac-lambda-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
+  name               = "iac-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+
+  lifecycle {
+    ignore_changes = [
+      assume_role_policy,
     ]
-  })
-
-  tags = {
-    Environment = "dev"
   }
-
-  # Add ignore_changes block to ignore errors if the role already exists
-  ignore_changes = ["name"]
 }
 
 resource "aws_iam_policy" "lambda_policy" {
-  name_prefix = "lambda_policy_"
-  policy      = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      }
-    ]
-  })
+  name_prefix = "iac-lambda-policy-"
 
-  # Add ignore_changes block to ignore errors if the policy already exists
-  ignore_changes = ["name_prefix"]
+  policy = data.aws_iam_policy_document.lambda_policy.json
+
+  lifecycle {
+    ignore_changes = [
+      policy,
+    ]
+  }
 }
