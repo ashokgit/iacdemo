c9d139821e19096412d8065caa59819b4e2d2416
ashok poudel <ashok.poudel@gmail.com>
2023-04-04 21:20:31 +0545

    hachuwa

--- main.tf
+++ main.tf
@@ -3,12 +3,16 @@ provider "aws" {
 }
 
 resource "aws_lambda_function" "transcription" {
+  count = var.create_lambda_function ? 1 : 0
   function_name = "audio-transcription"
   handler       = "transcribe_audio.lambda_handler" # Update the handler property
   runtime       = "python3.8"
   role          = var.create_iam_resources ? aws_iam_role.lambda_role[0].arn : data.aws_iam_role.existing_lambda_role[0].arn
 
   filename = "./lambda_function/lambda_function.zip"
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_lambda_permission" "apigw" {
