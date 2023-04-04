85a2ebbec421837c978e7bf5e5cfc46a3433d031
ashok poudel <ashok.poudel@gmail.com>
2023-04-04 21:27:11 +0545

    hachuwa

--- api_gateway.tf
+++ api_gateway.tf
@@ -23,7 +23,7 @@ resource "aws_api_gateway_integration" "transcription_integration" {
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.transcription.invoke_arn
+  uri                     = aws_lambda_function.transcription[0].invoke_arn
 }
 
 resource "aws_api_gateway_deployment" "transcription_deployment" {
