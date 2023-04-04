83355d34ba68996f57b914578d19b269b4500c72
ashok poudel <ashok.poudel@gmail.com>
2023-04-04 15:23:47 +0545

    removed duplicate roles

--- main.tf
+++ main.tf
@@ -11,22 +11,6 @@ resource "aws_lambda_function" "transcription" {
   filename = "./lambda_function/lambda_function.zip"
 }
 
-resource "aws_iam_role" "lambda_role" {
-  name = var.lambda_role_name
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
 resource "aws_lambda_permission" "apigw" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
